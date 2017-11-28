391ec487f93a6fe77f808190c78b8c6ee9601326
Christopher Arm <chrxssxdev@outlook.de>
2017-11-28 15:22:16 +0100

    Enable und Disable Message hinzugefügt

--- wirtschaftsserver/src/main/java/de/chrxssxdev/wserver/main/Main.java
+++ wirtschaftsserver/src/main/java/de/chrxssxdev/wserver/main/Main.java
@@ -1,17 +1,19 @@
 package de.chrxssxdev.wserver.main;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
-		
+		Bukkit.getConsoleSender().sendMessage("§aDas Plugin §6Wirtschaftsserver §awurde aktiviert§7!");
 	}
 	
 	@Override
 	public void onDisable() {
-		
+		Bukkit.getConsoleSender().sendMessage("§cDas Plugin §6Wirtschaftsserver §cwurde deaktiviert§7!");
+
 	}
 
 }
